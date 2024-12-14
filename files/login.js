024436a6424e883a72d36391bfb04dc09e9926cc
muhammadkashif20 <kashif28@gmail.com>
2024-12-15 01:04:23 +0500

    More Changes on UI & Working on js & firebase for solved more bugs upcoming complete!

--- files/login.js
+++ files/login.js
@@ -1,23 +1,21 @@
-import { app,auth,signInWithEmailAndPassword } from "./firebase.mjs";
-let loginBtn=document.getElementById('loginBtn');
-loginBtn.addEventListener('click',()=>{
-    
-    let loginEmail=document.getElementById('loginEmail')
-    let loginPassword=document.getElementById('loginPassword')
-    signInWithEmailAndPassword(auth, loginEmail.value, loginPassword.value)
+import { app, auth, signInWithEmailAndPassword } from "./firebase.mjs";
+let loginBtn = document.getElementById("loginBtn");
+loginBtn.addEventListener("click", () => {
+  let loginEmail = document.getElementById("loginEmail");
+  let loginPassword = document.getElementById("loginPassword");
+  signInWithEmailAndPassword(auth, loginEmail.value, loginPassword.value)
     .then((userCredential) => {
       const user = userCredential.user;
-      alert('Your Account is Login SuccessFully!')
+      alert("Your Account is Login SuccessFully!");
       console.log(user);
-      window.location.href='index.html';
+      window.location.href = "index.html";
     })
     .catch((error) => {
       const errorCode = error.code;
       console.log(errorCode);
-      alert('Try Again!')
-      alert(errorCode)
+      alert("Try Again!");
+      alert(errorCode);
       const errorMessage = error.message;
       console.log(errorMessage);
-      
     });
-})
+});
